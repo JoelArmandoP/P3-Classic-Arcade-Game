9823b7fd520db3ba40020b305f3d5c60a32ae183
Joel Armando <joelarma@gmail.com>
2015-09-06 15:59:28 +0100

    Add documentation to the new variables and functions

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -12,15 +10,13 @@ var Enemy = function() {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
     this.x = this.x + this.speed * dt;
     if (this.x > game.columns * game.colWidth) {
         this.place();
     }
 };
 
+// Place the enemy at a random position with a random speed
 Enemy.prototype.place = function() {
     this.x = -Math.floor(Math.random() * game.columns) * game.colWidth - game.colWidth;
     this.y = Math.floor(Math.random() * 3 + 1) * game.rowHeight - 20;
@@ -32,26 +28,31 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+
+// Player constructor
 var Player = function(x, y) {
     this.sprite = 'images/char-boy.png';
     this.x = x;
     this.y = y;
 }
 
-Player.prototype.update = function() {
+// It upates the position of the Player
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt) {
+    this.
 }
 
+// Draw the Player on the screen, required method for game
 Player.prototype.render = function() {
      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
-Player.prototype.handleInput = function() {
-
+// Change the Player target position in response to input
+// Parameter: Direction to move to
+Player.prototype.handleInput = function(dir) {
 }
 
+// Define the parameter for the board of the game and the amount of enemies
 var game = {
     rows: 6,
     columns: 5,
@@ -60,23 +61,22 @@ var game = {
     numEnemies: 6
 }
 
+// Keep track of all the enemies.
 var allEnemies = [];
+
+// Create the new enemies and push them into allEnemies
 for (var i = 0; i < game.numEnemies; i++) {
     var enemy = new Enemy();
     allEnemies.push(enemy);
 }
 
+// Create the Player for the game
 var player = new Player(300, 205);
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This listens for key presses and sends the keys to
+// Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
